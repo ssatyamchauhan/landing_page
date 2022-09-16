b0460f3f556710c340b2d3ee2d65649fed3e0474
Satyam Singh <officialsatyam1010@gmail.com>
2022-09-16 18:45:28 +0530

    fixed error permission policy issue while loading github.io host

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  kit: {
+    // etc...
+    floc: process.env.NODE_ENV === "development",
+  },
 })
